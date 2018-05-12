510449d36c310a7cabbec02a7e8600b869b4a758
MegaMinerd <MegaMinerd@gmail.com>
2018-05-11 21:22:11 -0400

    Changed appearance of waypoint markers

--- src/com/mega/pmds/gui/ScriptMapPanel.java
+++ src/com/mega/pmds/gui/ScriptMapPanel.java
@@ -1,10 +1,11 @@
 package com.mega.pmds.gui;
 
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -40,8 +41,14 @@ public class ScriptMapPanel extends ScriptContentPanel{
         		String loc = waypoint.substring(waypoint.indexOf("(")+1, waypoint.indexOf(")"));
         		int x = Integer.parseInt(loc.split(",")[0])*8;
         		int y = Integer.parseInt(loc.split(",")[1].trim())*8;
+        		g.setColor(Color.WHITE);
+        		g.fillOval(x-4, y-4, 8, 8);
+        		g.setFont(new Font("default", Font.BOLD, 16));
+        		g.drawString(Integer.toHexString(node.getIndex(child)), x+6, y+4);
+        		g.setColor(Color.BLACK);
         		g.drawLine(x-2, y, x+2, y);
         		g.drawLine(x, y-2, x, y+2);
+        		g.setFont(new Font("default", Font.PLAIN, 16));
         		g.drawString(Integer.toHexString(node.getIndex(child)), x+6, y+4);
         	}
         }
